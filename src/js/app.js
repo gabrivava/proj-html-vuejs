1f8f3bdd4de6ec59049dc9f865ce2dcbd55f0c6a
gabrivava <gabriele.vavassori4@gmail.com>
2021-06-16 09:14:43 +0200

    adding online courses as object in vue js

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,65 @@ const app = new Vue({
     data: {
         dataEvento: new Date("Jan 5, 2022 15:37:25").getTime(),
         timerEvento: '',
-        navList: ['Home', 'Pages', 'Courses' , 'Features', 'Blog', 'Shop']
+        navList: ['Home', 'Pages', 'Courses' , 'Features', 'Blog', 'Shop'],
+        courses: [
+            {
+                img: 'artist-course-08-480x480.jpg',
+                price: '$18.00',
+                title: 'The Acrylic Painting Academy',
+                lessons: '4',
+                students: '50',
+            },
+            {
+                img: 'artist-course-07-480x480.jpg',
+                price: '$21.00',
+                title: 'Drawing and Shading: Complete Course',
+                lessons: '14',
+                students: '50',
+            },
+            {
+                img: 'artist-course-06-480x480.jpg',
+                price: '$19.00',
+                title: 'The Color Theory for Digital Artist',
+                lessons: '7',
+                students: '50',
+            },
+            {
+                img: 'artist-course-05-480x480.jpg',
+                price: '$35.00',
+                title: 'Ultimate Guide Digital Sketching for Beginner',
+                lessons: '14',
+                students: '50',
+            },
+            {
+                img: 'artist-course-04-480x480.jpg',
+                price: '$19.00',
+                title: 'Portrait Drawing The Smart Way',
+                lessons: '2',
+                students: '50',
+            },
+            {
+                img: 'artist-course-03-480x480.jpg',
+                price: '$19.00',
+                title: 'Mastering Watercolor Painting from Beginner',
+                lessons: '9',
+                students: '50',
+            },
+            {
+                img: 'artist-course-02-480x480.jpg',
+                price: '$25.00',
+                title: 'The Art & Science of Drawing',
+                lessons: '4',
+                students: '50',
+            },
+            {
+                img: 'artist-course-01-480x480.jpg',
+                price: '$22.00',
+                title: 'The Colored Pencil Drawing Course',
+                lessons: '6',
+                students: '50',
+            },
+        ]
     },
     methods: {
             
